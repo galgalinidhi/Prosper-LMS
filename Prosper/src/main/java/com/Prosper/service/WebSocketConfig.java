de333893eb4153d2fff84aa378f7fbb5f9549d4b
Joshi, Himanshu <hsjoshi@iu.edu>
2022-04-23 16:03:17 -0400

    recent Code changes for chat

--- Prosper/src/main/java/com/Prosper/service/WebSocketConfig.java
+++ Prosper/src/main/java/com/Prosper/service/WebSocketConfig.java
@@ -15,7 +15,7 @@ import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     	registry.addEndpoint("/ws")
     	.setHandshakeHandler(new DefaultHandshakeHandler(new TomcatRequestUpgradeStrategy()))
     	.withSockJS();
