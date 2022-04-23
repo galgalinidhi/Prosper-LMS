73046d61d7f5089750c665f73af6248a7ad25ff7
Joshi, Himanshu <hsjoshi@iu.edu>
2022-04-23 15:50:47 -0400

    recent Code changes for chat

--- Prosper/src/main/java/com/Prosper/controller/UserController.java
+++ Prosper/src/main/java/com/Prosper/controller/UserController.java
@@ -6,8 +6,10 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,12 +34,20 @@ import lombok.extern.log4j.Log4j2;
 @RestController
 @CrossOrigin
 @RequestMapping("/user")
+@EnableOAuth2Sso
+@EnableResourceServer 
 public class UserController {
 	
 	private static final Logger logger = LogManager.getLogger(UserController.class);
 	
 	@Autowired
 	private UserService userService;
+	
+    @RequestMapping("/")
+    String home(java.security.Principal user) {
+        return "Hello " + user.getName();
+    }
+
 	
 	@PostMapping("/register")
 	public ResponseEntity<UserResponse> userData(@RequestBody UserRequest userRequestModel) {
--- Prosper/src/main/java/com/Prosper/service/WebSocketConfig.java
+++ Prosper/src/main/java/com/Prosper/service/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.Prosper.service;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -9,6 +10,7 @@ import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrate
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
+@Component
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
